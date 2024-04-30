a5222c27aa43f8ae57bab085ec277e0dff992e95
mts_sc <matheuscostac44@gmail.com>
2024-04-29 22:34:16 -0300

    commit

--- View/Project1.dpr
+++ View/Project1.dpr
@@ -2,11 +2,13 @@ program Project1;
 
 uses
   System.StartUpCopy,
-  FMX.Forms;
+  FMX.Forms,
+  UnitLogin in 'E:\projetos\pet\View\UnitLogin.pas' {FrmLogin};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TFrmLogin, FrmLogin);
   Application.Run;
 end.
